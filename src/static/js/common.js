bb237fffb3799c91ead7aeae430b9f4e570d0c30
Aslan <berezg86@gmail.com>
2020-11-13 12:28:48 +0300

    Стилизация шапки

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,52 @@
 $(document).ready(function () {
-    // alert(1);
+    $('.burger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdown-mob').toggleClass('is-active');
+    });
+
+    $('.menubtn').click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdowndesc').fadeToggle();
+    });
+
+
+
+    var contSlider = new Swiper('.adress-slider', {
+        slidesPerView: 7,
+        spaceBetween: 30,
+        loop: true,
+        observer: true,
+        observeParents: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 1.4,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2.2,
+                spaceBetween: 20,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1599: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+            1799: {
+                slidesPerView: 6,
+                spaceBetween: 20,
+            }
+        }
+    });
+
 });
