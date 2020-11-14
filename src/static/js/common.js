78d845635d53a1c9f65e3863949cf3dce359951d
Aslan <berezg86@gmail.com>
2020-11-14 14:29:55 +0300

    Верстка всех блоков

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -89,4 +89,80 @@ $(document).ready(function () {
         }
     });
 
+    var myAdvSlider = undefined;
+    function initSwiper() {
+        var screenWidth = $(window).width();
+        if (screenWidth > 991 && myAdvSlider == undefined) {
+
+            var myAdvSlider = new Swiper('.advgallery', {
+                slidesPerView: 5,
+                spaceBetween: 60,
+                loop: true,
+                observer: true,
+                observeParents: true,
+                navigation: {
+                    nextEl: '.swiper-button-next-adv',
+                    prevEl: '.swiper-button-prev-adv',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    1279: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    1599: {
+                        slidesPerView: 3,
+                        spaceBetween: 50,
+                    },
+                }
+            });
+        } else if (screenWidth < 992 && myAdvSlider != undefined) {
+            myAdvSlider.destroy();
+            myAdvSlider = undefined;
+        }
+    }
+
+    initSwiper();
+
+    $('.selectform select').selectric();
+
+    var lislider = new Swiper('.ourlisence-slider', {
+        slidesPerView: 6,
+        spaceBetween: 60,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next-n',
+            prevEl: '.swiper-button-prev-p',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            599: {
+                slidesPerView: 1.4,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1599: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+            },
+        }
+    });
+
 });
